fcd4db95696407c1722ec1047e8d98ec429ffe02
Liam Schumm <liamschumm@icloud.com>
2016-08-09 10:58:58 -0500

    Flask

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def update():
+    return "Well hello there!"
+
+@app.route("/", methods=["PUT"])
+def send():
+    return "FEED ME SEYMOUR!!!"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return "Page/File not found", 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
