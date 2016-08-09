f0bb9088ce21d88112fbc34d0bf65feb66ca80c1
Liam Schumm <liamschumm@icloud.com>
2016-08-09 12:24:55 -0500

    Return correct stuff

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/", methods=["POST"])
 @cross_origin()
 def update():
-    return randint(1,16)
+    return str(randint(1,16))
 
 @app.route("/", methods=["PUT"])
 def send():
