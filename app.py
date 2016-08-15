edee9046c98b471aed9035264c63abaa2ffd9678
Liam Schumm <liamschumm@icloud.com>
2016-08-15 12:10:59 -0500

    Initial (working) commit: 	- Deployed on Heroku 	- Accepts POST requests (would be used for controlling the test), and returns a random number between 1-16 (inclusive) 	- Accepts PUT requests (would be used for collecting anonymous data), returns the message "FEED ME, SEYMOUR!!!" 	- Accepts GET requests (for testing), returns the message "have a good day!"

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.cors import CORS, cross_origin
+from random import randint
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/", methods=["POST"])
+@cross_origin()
+def update():
+    return str(randint(1,16))
+
+@app.route("/", methods=["PUT"])
+def send():
+    # send records for archiving
+    return "FEED ME, SEYMOUR!!!"
+
+@app.route("/", methods=["GET"])
+def do():
+    return "have a good day!"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return "Page/File not found", 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
